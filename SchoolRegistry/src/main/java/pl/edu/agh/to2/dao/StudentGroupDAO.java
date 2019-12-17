583a8c920b8a178204e356e69b16b60dcf025afa
Agnieszka Staroń <astaron98@gmail.com>
2019-12-17 01:25:51 +0100

    Update/delete/findAll functions added to Dao classes

--- SchoolRegistry/src/main/java/pl/edu/agh/to2/dao/StudentGroupDAO.java
+++ SchoolRegistry/src/main/java/pl/edu/agh/to2/dao/StudentGroupDAO.java
@@ -1,5 +1,6 @@
 package pl.edu.agh.to2.dao;
 
+import org.hibernate.Query;
 import pl.edu.agh.to2.model.Student;
 import pl.edu.agh.to2.model.StudentGroup;
 import pl.edu.agh.to2.model.Teacher;
@@ -9,6 +10,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 import javax.persistence.PersistenceException;
 import java.util.Date;
+import java.util.List;
 
 public class StudentGroupDAO extends GenericDAO<StudentGroup> {
 
@@ -22,13 +24,31 @@ public class StudentGroupDAO extends GenericDAO<StudentGroup> {
         }
     }
 
+    public void update(int studentGroupId, String name, Teacher tutor){
+        SessionService.getSession().createQuery("UPDATE StudentGroup s set s.name = :name "
+                + "WHERE s.studentGroupId = :studentGroupId").setParameter("studentGroupId", studentGroupId)
+                .setParameter("name", name).executeUpdate();
+        SessionService.getSession().createQuery("UPDATE StudentGroup s set s.tutor = :tutor "
+                + "WHERE s.studentGroupId = :studentGroupId").setParameter("studentGroupId", studentGroupId)
+                .setParameter("tutor", tutor).executeUpdate();
+    }
+
+    public int delete(int studentGroupId){
+        Query query = SessionService.getSession().createQuery("DELETE FROM StudentGroup s WHERE s.studentGroupId = :studentGroupId")
+                .setParameter("studentGroupId", studentGroupId);
+        int result = query.executeUpdate();
+        System.out.println("Rows affected: " + result);
+        return result;
+    }
+
+    public List<StudentGroup> findAll(){
+        Query query = SessionService.getSession().createQuery("FROM StudentGroup s");
+        return query.list();
+    }
+
     public StudentGroup findByName(String name){
-        EntityManager em = SessionService.getEntityManager();
-        EntityTransaction etx = SessionService.getEntityTransaction();
-        etx.begin();
-        StudentGroup studentGroup = em.createQuery("SELECT s FROM StudentGroup s WHERE s.name = :name", StudentGroup.class)
-                .setParameter("name", name).getSingleResult();
-        etx.commit();
+        StudentGroup studentGroup = (StudentGroup) SessionService.getSession().createQuery("SELECT s FROM StudentGroup s WHERE s.name = :name")
+                .setParameter("name", name).list().get(0);
         return studentGroup;
     }
 
--- SchoolRegistry/src/main/java/pl/edu/agh/to2/dao/TeacherDAO.java
+++ SchoolRegistry/src/main/java/pl/edu/agh/to2/dao/TeacherDAO.java
@@ -1,5 +1,6 @@
 package pl.edu.agh.to2.dao;
 
+import org.hibernate.Query;
 import pl.edu.agh.to2.model.Student;
 import pl.edu.agh.to2.model.Teacher;
 import pl.edu.agh.to2.session.SessionService;
@@ -8,6 +9,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 import javax.persistence.PersistenceException;
 import java.util.Date;
+import java.util.List;
 
 public class TeacherDAO extends GenericDAO<Teacher> {
 
@@ -21,10 +23,44 @@ public class TeacherDAO extends GenericDAO<Teacher> {
         }
     }
 
+    public void update(int teacherId, String firstName, String lastName, String phone, String email, String password){
+        SessionService.getSession().createQuery("UPDATE Teacher t set t.firstName = :firstName "
+                + "WHERE t.teacherId = :teacherId").setParameter("teacherId", teacherId).setParameter("firstName", firstName).executeUpdate();
+        SessionService.getSession().createQuery("UPDATE Teacher t set t.lastName = :lastName "
+                + "WHERE t.teacherId = :teacherId").setParameter("teacherId", teacherId).setParameter("lastName", lastName).executeUpdate();
+        SessionService.getSession().createQuery("UPDATE Teacher t set t.phone = :phone "
+                + "WHERE t.teacherId = :teacherId").setParameter("teacherId", teacherId).setParameter("phone", phone).executeUpdate();
+        SessionService.getSession().createQuery("UPDATE Teacher t set t.email = :email "
+                + "WHERE t.teacherId = :teacherId").setParameter("teacherId", teacherId).setParameter("email", email).executeUpdate();
+        SessionService.getSession().createQuery("UPDATE Teacher t set t.password = :password "
+                + "WHERE t.teacherId = :teacherId").setParameter("teacherId", teacherId).setParameter("password", password).executeUpdate();
+    }
+
+    public int delete(int id){
+        Query query = SessionService.getSession().createQuery("DELETE FROM Teacher t WHERE t.teacherId = :id")
+                .setParameter("id", id);
+        int result = query.executeUpdate();
+        System.out.println("Rows affected: " + result);
+        return result;
+    }
+
+    public List<Teacher> findAll(){
+        Query query = SessionService.getSession().createQuery("FROM Teacher t");
+        return query.list();
+    }
+
     public Teacher findByTeacherId(int teacherId){
 
-        return (Teacher) SessionService.getSession().createQuery("SELECT t FROM Teacher t WHERE t.teacherId = :id")
+        Teacher teacher = (Teacher) SessionService.getSession().createQuery("SELECT t FROM Teacher t WHERE t.teacherId = :id")
                 .setParameter("id", teacherId).list().get(0);
+        return teacher;
+    }
+
+    public Teacher findByTeacherName(String firstName, String lastName){
+
+        Teacher teacher = (Teacher) SessionService.getSession().createQuery("SELECT t FROM Teacher t WHERE t.firstName = :firstName AND t.lastName = :lastName")
+                .setParameter("firstName", firstName).setParameter("lastName", lastName).list().get(0);
+        return teacher;
     }
 
 
